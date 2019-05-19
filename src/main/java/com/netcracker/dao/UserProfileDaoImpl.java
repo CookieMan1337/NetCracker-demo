6e069dba96bd930a540844ad08739c5f767546ec
CookieMan1337 <anna.kodeh@gmail.com>
2019-05-20 02:02:31 +0300

    Initial commit

---
+++ src/main/java/com/netcracker/dao/UserProfileDaoImpl.java
@@ -0,0 +1,31 @@
+package com.netcracker.dao;
+
+import com.netcracker.model.UserProfile;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("userProfileDAO")
+public class UserProfileDaoImpl extends AbstractDao<Long, UserProfile> implements UserProfileDao {
+
+    public UserProfile findById(long id) {
+        return getByKey(id);
+    }
+
+    public UserProfile findByType(String type) {
+        Criteria crit = createEntityCriteria();
+        crit.add(Restrictions.eq("type", type));
+        return (UserProfile) crit.uniqueResult();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<UserProfile> findAll() {
+        Criteria crit = createEntityCriteria();
+        crit.addOrder(Order.asc("type"));
+        return (List<UserProfile>) crit.list();
+    }
+
+}
